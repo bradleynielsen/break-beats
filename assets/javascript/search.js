f699a33b134f8921dd1aa7863d475bb0226eefc7
bradleynielsen <bradleynielsen@gmail.com>
2016-09-24 17:02:04 -0500

    trying new commit message; added search stuff; playlist listing still broken.

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAauOC4PC5WrE3Br4Ff1-HkA-FxBmh1QfQ",
+    authDomain: "breakbeats-4758a.firebaseapp.com",
+    databaseURL: "https://breakbeats-4758a.firebaseio.com",
+    storageBucket: "breakbeats-4758a.appspot.com",
+    messagingSenderId: "1058322946724"
+  };
+  firebase.initializeApp(config);
+
+
+function build tagsArray() {
+var tagsArray = firebase.database().ref(/tagsArray)
+
+
+
 	
+}
+
+
 var tags = [
       "funny",
       "calm",
@@ -11,8 +32,6 @@ var tags = [
       "fast",
       "cats",
     ];
-
-
 
 var breaks = [
 

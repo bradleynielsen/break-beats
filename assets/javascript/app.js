33475efcf3766a1f87bdb8eedf8986c083c71869
Aly Doerscher <doerschera@gmail.com>
2016-09-27 21:18:38 -0500

    consolidated final files (#25)
    * sync (#16)
    
    ok
    
    * added images folder in assets
    
    * added images folder in assets (#18)
    
    * Revert "added images folder in assets" (#19)
    
    * created index and style files
    
    * style and js links
    
    * sass color and font variables
    
    * laning page structure
    
    * landing page style
    
    * search playlist structure
    
    * playlist searchbar style
    
    * playlist display
    
    * playlist hover style
    
    * youtube search basic html
    
    * video search styling
    
    * send page basic html
    
    * send page style
    
    * search bar and button styling
    
    * add tag chips playlist search
    
    * add tags review send page
    
    * navigate fromlanding page
    
    * append searched videos
    
    * added radio buttons
    
    * list selected videos
    
    * add chips
    
    * delete tag
    
    * playlist display styling
    
    * display chosen videos in playlist
    
    * close review and send screen
    
    * landing page animation
    
    * search tag for playlist
    
    * playlist search display
    
    * yt search load images
    
    * remove selected videos
    
    * playlist hover
    
    * consolidated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,32 +16,48 @@ var BB = (function() {
 	  var dbRef = firebase.database().ref();
 	// create a reference to the 'links' child inside db
 	  playlistsRef = dbRef.child('playlists');
-	  console.log(playlistsRef);
+	// create a reference to the 'tags' child
+    var tagsRef = dbRef.child('tags');
 
-	  playlistsRef.on('value', function(snapshot) {
-	    var playlists = snapshot.val();
-	    for(var playlist in playlists) {
-	      console.log(atob(playlist));
-	    }
-	  });
+	/*--------------------------------------------
+	 * email init
+	 * ---------------------------------------- */
+
+	emailjs.init("user_xrCuLVJF1XPydVeSaCraO");
 
 	/* -------------------------------------------
 	 * Bild DOM
 	 * ---------------------------------------- */
 
   // cache DOM
-  var $submitButton = $('#submit'),
-      $videosContainer = $('#player'),
-      $listContainer = $('.Selected-list'),
-      $searchInput = $('#search-input'),
+  var $createPlaylist = $('#js-create-playlist'),
+			$ytSearch = $('#js-yt-search-button'),
+			$playlistSearch = $('#js-playlist-search'),
+			$playlistSearchButton = $('#js-playlist-search-button'),
+      $videosContainer = $('.yt-results'),
+      $listContainer = $('#js-selected-list'),
+			$landingSearch = $('#js-landing-search'),
+      $ytSearchInput = $('#js-yt-search'),
+			$viewNewPlaylist = $('#js-view-new-playlist'),
       $saveButton = $('.save-playlist'),
-      $playlistName = $('.playlist-name'),
+			$sendButton = $('#js-send-email'),
+      $playlistTitle = $('#js-playlist-name'),
+			$playlistVideoContainer = $('#js-video-list'),
+			$newTags = $('#js-user-tags'),
+			$addTag = $('#js-add-tag-button'),
+			$removeTag = $('.chip'),
+			$close = $('#js-close-send-review'),
+			$magenta = '#AB537F',
       query,
       searchTerm,
       videoId,
+			returnedTags= [],
       titles = [],
+			userTags = [],
       checkedBoxes,
-      maxResults = 5,
+      maxResults = 10,
+			videoCounter = 0,
+			playlistCounter =0,
       paginationData;
 
 	// render DOM
@@ -51,22 +67,95 @@ var BB = (function() {
 	  function renderSelectedTitles(titles) {
 	    $listContainer.html('');
 	    for(var i = 0; i < titles.length; i++) {
-
-	      $listContainer.append('<h3>'+ titles[i].title +'<h3>');
+	      $listContainer.append('<li class="remove-video">'+ titles[i].title +'&nbsp<i class="tiny close material-icons">close</i><li>');
 	    }
-	    
-	  }
 
+	  }
+		function renderNewTags(tag) {
+			$newTags.append(tag);
+		}
+		function renderNewPlaylist() {
+			for(var i = 0; i < titles.length; i++) {
+				var title = titles[i].title;
+				var image = titles[i].image;
+
+				var $li = $('<li>');
+				var $wrapperDiv = $('<div class="col s12"></div>');
+				$wrapperDiv.append('<img class="col s6" src='+image+' />');
+				$wrapperDiv.append('<p class="flow-text col s6">'+title+'</p>');
+				$li.append($wrapperDiv);
+				$playlistVideoContainer.append($li);
+
+			}
+		}
+
+	// DOM show/hide
+	function hideLanding(pageToShow) {
+		$('.landing-page').addClass('disable');
+		$(pageToShow).removeClass('disable');
+	}
+		function reviewAndSend() {
+			$('.search-yt').addClass('opacity');
+			$('.playlist-review-send').removeClass('disable');
+		}
+		function closeReviewSend() {
+			$('.playlist-review-send').addClass('disable');
+			$('.search-yt').removeClass('opacity');
+		}
+		function playlistHover() {
+			$(this).append('<div class="playlist-hover"></div>');
+			var index = $(this).index();
+			var height = $(this).height();
+			var width = $(this).width();
+			$('.playlist-hover').css({
+				'height': height,
+				'width': width
+			})
+			$('.playlist-hover').append('<h4>This playlist is...</h4>');
+			renderPlaylistTags(index);
+			$('.playlist-hover').append('<a class="waves-effect btn" id="js-view-playlist">view</a>');
+		}
+		function renderPlaylistTags(index) {
+			console.log(returnedTags);
+			var tagArray = returnedTags[index];
+			for(var i = 0; i < tagArray.length; i++) {
+				var tag = tagArray[i];
+				var tagDiv = $('<div class="chip">'+tag+'</div>');
+				$('.playlist-hover').append(tagDiv);
+			}
+		}
 
 	// bind events
-	  $submitButton.on('click', doSearch);
-	  $saveButton.on('click', getTitle);
+	  $ytSearch.on('click', doSearch);
+		$sendButton.on('click', getPlaylistTitle);
+	  $saveButton.on('click', getPlaylistTitle);
+		$createPlaylist.on('click', function() {
+			hideLanding('.search-yt');
+		});
+		$landingSearch.on('click', function() {
+			hideLanding('.search-playlists');
+		});
+		$viewNewPlaylist.on('click', function() {
+			reviewAndSend();
+			renderNewPlaylist();
+		});
+		$addTag.on('click', addTag);
+		$(document).on('click', '.remove-tag', removeTag);
+		$close.on('click', closeReviewSend);
+		$playlistSearchButton.on('click', searchTags);
+		$listContainer.on('click', '.remove-video', removeVideo);
+		$('.playlist-results').on('mouseenter', '.playlist-display', playlistHover);
+		$('.playlist-results').on('mouseleave', '.playlist-display', function() {
+			$('.playlist-hover').remove();
+		})
+
 
 
 
 	// search function
 	  function doSearch() {
-	    searchTerm = $searchInput.val().replace(/ /g, '+');
+			$videosContainer.empty();
+	    searchTerm = $ytSearchInput.val().replace(/ /g, '+');
 	    query = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=' + maxResults + '&order=viewCount&q="' + searchTerm + '"&type=video&key=AIzaSyDIE0dd7hZ5j4vQRtwrU0CwQLGq-lhXWCc';
 	    getDatafromAPI(query);
 	    return false;
@@ -78,28 +167,90 @@ var BB = (function() {
 	      method: 'GET'
 	    }).done( data => {
 	      // videoId = data[0].id.videoId;
-	      console.log(data.items);
 	      for(var i = 0; i < data.items.length; i++) {
-	        createIframe(data.items[i]);
+	        createImage(data.items[i]);
 	      }
 	    });
 	  }
 
-	// create iframe fucntion from data
-	  function createIframe(item) {
-	    var videoContainer = $('<div>').addClass('Video-container');
+
+		/* -------------------------------------------
+	   * create iframe function from data
+	   * ---------------------------------------- */
+
+		function createIframe(item) {
+	    var videoContainer = $('<div>').addClass('video col s6');
 	    var video = $('<iframe />', {
 	      class: 'Video-iframe Video-iframe--loading',
-	      width: '400',
-	      height: '225',
+	      width: '100%',
+				height: '100%',
 	      src: 'https://www.youtube.com/embed/'+item.id.videoId+'?rel=0',
 	      frameborder: '0'
 	    });
 	    // var checkbox = '<label class="checkbox" for="' + videoId + '"><input type="checkbox" checked="checked" value="" id="' + videoId + '" data-toggle="checkbox" class="custom-checkbox"><span class="icons"><span class="icon-unchecked"></span><span class="icon-checked"></span></span></label>';
-	    var checkbox = '<input type="checkbox" class="checkbox" id=' + item.id.videoId + ' data-title="' + item.snippet.title + '" data-image="' + item.snippet.thumbnails.default.url + '" class="checkbox" />';
-	    videoContainer.append(video).append(checkbox);
+	    var checkbox = '<input type="radio" class="with-gap radio" id=' + item.id.videoId + ' data-title="' + item.snippet.title + '" data-image="' + item.snippet.thumbnails.default.url + '" />';
+			var label = '<label for='+item.id.videoId+'></label>';
+	    videoContainer.append(video).append(checkbox).append(label);
 	    renderVideos(videoContainer);
 	  }
+
+		function createImage(item) {
+			var imgContainer = $('<div>').addClass('video col s6');
+			var title = '<h5>' + item.snippet.title + '</h5>'
+			var image = $('<img>', {
+				class: 'Image',
+				width: '100%',
+				height: '100%',
+				src: item.snippet.thumbnails.default.url
+			});
+			var checkbox = '<input type="radio" class="with-gap radio" id=' + item.id.videoId + ' data-title="' + item.snippet.title + '" data-image="' + item.snippet.thumbnails.default.url + '" />';
+			var label = '<label for='+item.id.videoId+'></label>';
+			imgContainer.prepend(title).append(image).append(checkbox).append(label);
+			renderVideos(imgContainer);
+		}
+
+		function removeVideo() {
+			console.log(titles);
+			var title = $(this).text()
+			title = title.replace(/close/g, "");
+			console.log(title);
+			var index = $(this).index();
+			console.log(index);
+			if(index === 0) {
+				$listContainer.children('li').eq(0).remove();
+				titles.splice(0, 1);
+			} else if(index === 2) {
+				$listContainer.children('li').eq(2).remove();
+				titles.splice(1, 1);
+			} else {
+				$listContainer.children('li').eq(4).remove();
+				titles.splice(2, 1);
+			}
+			$('.radio').prop('checked', false);
+			$('.radio').prop('disabled', false);
+			console.log(titles);
+		}
+
+
+		// internal tag search
+		function searchResultsTitle(title) {
+			var newDiv = $('<div class="playlist-display col s4" id="playlist'+playlistCounter+'">');
+			var h3 = $("<h3>"+title+"</h3>");
+			$('.playlist-results').append(newDiv);
+			$(newDiv).append(h3);
+			$(newDiv).append('<ul>')
+		}
+
+		function searchResultsList(image, title) {
+			var $li = $('<li>');
+			var $wrapperDiv = $('<div class="col s12"></div>');
+			$wrapperDiv.append('<img class="col s6" src='+image+' />');
+			$wrapperDiv.append('<p class="flow-text col s6">'+title+'</p>');
+			$li.append($wrapperDiv);
+			$('#playlist'+playlistCounter).children("ul").append($li);
+			$('.playlist-display').css('border', 'solid 1px '+$magenta);
+
+		}
 
 
 
@@ -126,7 +277,7 @@ var BB = (function() {
 
 
 
-  $(document).on('change', '.checkbox', addVideoToPlaylist);
+  $(document).on('change', '.radio', addVideoToPlaylist);
 
   function addVideoToPlaylist() {
     if ( $(this).is(":checked") ) {
@@ -135,8 +286,8 @@ var BB = (function() {
         videoId     = $(this).attr('id');
         videoImg    = $(this).data('image');
         titles.push({
-          'title': videoTitle, 
-          'image': videoImg, 
+          'title': videoTitle,
+          'image': videoImg,
           'videoId': videoId
         });
         renderSelectedTitles(titles);
@@ -155,7 +306,7 @@ var BB = (function() {
   }
 
   function disableSelectionIfPlaylistFull() {
-    checkedBoxes = $(document).find('.checkbox');
+    checkedBoxes = $(document).find('.radio');
     if ( titles.length > 2 ) {
       for(var i = 0; i < checkedBoxes.length; i++) {
         if( checkedBoxes[i].checked == false) {
@@ -166,7 +317,7 @@ var BB = (function() {
   }
 
   function enableSelection() {
-    checkedBoxes = $(document).find('.checkbox');
+    checkedBoxes = $(document).find('.radio');
     for(var i = 0; i < checkedBoxes.length; i++) {
       if( checkedBoxes[i].disabled == true) {
         checkedBoxes[i].disabled = false;
@@ -181,29 +332,92 @@ var BB = (function() {
    * ---------------------------------------- */
 
 
-  function getTitle() {
-    var playlistName = btoa($playlistName.val().trim());
+  function getPlaylistTitle() {
+    var playlistTitle = $playlistTitle.val().trim();
+    var tags = userTags;
+
+    var newPlaylist = playlistsRef.push().key;
+    playlistsRef.child('/'+newPlaylist+'/').update(
+      {
+        'vtitle': playlistTitle,
+        'vtags': tags
+      });
+
     for(i = 0; i < titles.length; i++) {
-      saveToFirebase(playlistName, titles[i]);
+      saveToFirebase(newPlaylist, titles[i]);
     }
+
+    // var emailAddress = $emailAddress.val().trim();
+    // sendEmail(emailAddress, newPlaylist);
+
+
+    // global tag array
+    // for(i = 0; i < tags.length; i++) {
+    //   tagsRef.child('/'+tags[i]+'/').set({
+    //     'tags': 'tag'
+    //   });
+    // }
+
+
     return false;
   }
 
+	function addTag() {
+		var newTag = $('#js-add-tags').val().trim();
+		var newChip =$('<div class="chip">'+newTag+'</div>');
+		var iTag = $('<i class="close material-icons remove-tag">close</i>');
+		newChip.append(iTag);
+		$('#js-user-tags').append(newChip);
+
+		userTags.push(newTag);
+		console.log(userTags);
+	}
+
+	function removeTag() {
+		var tag = $(this).parent().text();
+		tag = tag.replace(/close/i, "");
+		var index = userTags.indexOf(tag);
+		userTags.splice(index, 1);
+		console.log(userTags);
+	}
+
   function saveToFirebase(playlistName, video) {
-    // add to firebase
-    // firebase.database().ref('/playlists/'+playlistName).update({
-    //   videoId: selectedVideoId,
-    //   defaultImg: selectedVideoImg
-    // });
     vid = video.videoId;
     vimg = video.image;
-    playlistsRef.child(playlistName).push({
+		vtitle = titles[videoCounter].title
+    playlistsRef.child(playlistName).child('videos').push({
         videoId: vid,
-        defaultImg: vimg
-    });    
+        defaultImg: vimg,
+    });
   }
-  
+
   console.log(titles);
 
+	function searchTags() {
+		playlistsRef.once('value').then(function(snapshot) {
+			var tagSearch = $playlistSearch.val().trim()
+			console.log(tagSearch);
+
+			snapshot.forEach(function(childSnapshot) {
+				var playlist = childSnapshot.val();
+				var videos = playlist.videos;
+				var tags = playlist.vtags;
+
+				if(tags.indexOf(tagSearch) != -1) {
+					console.log(true);
+					var title = playlist.vtitle;
+					searchResultsTitle(title)
+					returnedTags.push(tags);
+					for(i in videos) {
+						var defaultImg = videos[i].defaultImg;
+						var videoId = videos[i].videoId;
+						var videoTitle = videos[i].videoTitle;
+						searchResultsList(defaultImg, videoTitle);
+					}
+				}
+
+			})
+		})
+	}
 
 })();
